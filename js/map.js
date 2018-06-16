c4e591985eb741fe9753fecf6517cbcde0a8ec83
Alexs222 <aleks.nikulin@gmail.com>
2018-06-16 09:11:39 +0200

    добавил активацию страницы

--- js/map.js
+++ js/map.js
@@ -6,6 +6,8 @@ var TIME_IN_OUT = ['12:00', '13:00', '14:00'];
 var FACILITIES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var COUNT_POINTS = 8;
+var MAP_PIN_WIDTH = 50;
+var MAP_PIN_HEIGHT = 70;
 var POINT_WIDTH = 50;
 var POINT_HEIGHT = 70;
 var MAX_WIDTH = 900;
@@ -43,17 +45,6 @@ var randomStrGenerator = function (arrStr) {
   }
   return stringTotal;
 };
-
-// var randomOrderGenerator = function (arrOrder) {
-//   var orderArr = [];
-//   while (orderArr.length < arrOrder.length) {
-//     var order = arrOrder[Math.floor(Math.random() * arrOrder.length)];
-//     if (!(orderArr.includes(order))) {
-//       orderArr.push(order);
-//     }
-//   }
-//   return orderArr;
-// };
 
 var randomDataGenerator = function () {
   var ads = [];
@@ -109,7 +100,7 @@ var createBlock = function (points) {
 };
 
 var adsData = randomDataGenerator();
-similarMapPinsElement.appendChild(createBlock(adsData));
+// similarMapPinsElement.appendChild(createBlock(adsData));
 
 // Создаем объявление
 var oneElementData = adsData[0];
@@ -207,4 +198,38 @@ var renderMapCard = function (card) {
   return cardElement;
 };
 
-mapElement.insertBefore(renderMapCard(oneElementData), mapFiltersContainerElement);
+// mapElement.insertBefore(renderMapCard(oneElementData), mapFiltersContainerElement);
+
+// Делаем поля не активными
+var fieldsetElements = document.querySelectorAll('fieldset');
+var disabledElementFormArr = Array.from(fieldsetElements);
+for (var i = 0; i < disabledElementFormArr.length; i++) {
+  disabledElementFormArr[i].setAttribute('disabled', 'disabled');
+}
+
+// Активация страницы
+var buttonActivation = document.querySelector('.map__pin--main');
+
+var coordMapPin = {
+  'x': buttonActivation.style.left,
+  'y': buttonActivation.style.top
+};
+var formAd = document.querySelector('.ad-form');
+var inputAddress = document.querySelector('#address');
+
+// Функция обработчик события mouseup на элементе map__pin--main
+var buttonActivationMouseupHandler = function () {
+  mapElement.classList.remove('map--faded');
+  formAd.classList.remove('.ad-form--disabled');
+  for (var j = 0; j < disabledElementFormArr.length; j++) {
+    disabledElementFormArr[j].removeAttribute('disabled');
+  }
+  inputAddress.value = (parseInt(coordMapPin.x, 10) - MAP_PIN_WIDTH / 2) + ', ' + (parseInt(coordMapPin.y, 10) - MAP_PIN_HEIGHT); // Учитываем ширину метки 62 / 2 и высоту метки 62 + 22
+  similarMapPinsElement.appendChild(createBlock(adsData));
+};
+
+
+buttonActivation.addEventListener('mouseup', buttonActivationMouseupHandler);
+
+// var
+// for (i = 0; i < )
